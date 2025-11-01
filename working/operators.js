40e2211e567277688f3e296e21e5f88450860aa4
nareshnaresh15801-ship-it <nareshnaresh15801@gmail.com>
2025-11-01 21:20:01 +0530

    Day3

---
+++ working/operators.js
@@ -0,0 +1,49 @@
+// let a =10
+// let b =20
+// // console.log(a+b);
+// // console.log(a-b);
+// // console.log(a*b);
+// // console.log(a%b);
+// // console.log(a/b);
+// // console.log(a**b);
+
+// // console.log(a==b);
+// // console.log(a===b);
+// // console.log(a>b);
+// // console.log(a+=b);
+// // console.log(a-=b);
+// // console.log(a/=5);
+// console.log(a**=2); 
+
+
+// function greet(){
+//     console.log("hello world to js !");
+
+// }
+
+// greet();
+
+// function greetuser(name){
+//     console.log("hello + namee+");
+// }
+
+// function add(a,b){
+//   return  a+b
+// }
+
+// IIRF
+
+// (function (){
+//     console.log("this is immediatly");
+// })
+
+let Numbers = [1,2,3,4,5,6,7,8,9,]
+let even = Numbers.filter(n => n% 2 );
+console.log(even);
+
+
+
+
+
+ 
+  
