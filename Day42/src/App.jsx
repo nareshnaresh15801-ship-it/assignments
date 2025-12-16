c5b9c766f9d1913a7c92235a623d37969dff22f8
nareshnaresh15801-ship-it <nareshnaresh15801@gmail.com>
2025-12-16 20:54:57 +0530

    Day42

---
+++ Day42/src/App.jsx
@@ -0,0 +1,48 @@
+
+import './App.css'
+import Counter from './Day 42/Task-1'
+import TextDisplay from './Day 42/Task-2'
+import Toggole from './Day 42/Task-3'
+import EvenOddChecker from './Day 42/Task-4'
+import ColorButton from './Day 42/Task-5'
+import LoginForm from './Day 42/Task-6'
+import ThemeSwitcher from './Day 42/Task-7'
+import Counter02 from './Day 42/Task-8'
+import DropdownMenu from './Day 42/Task-9'
+import TodoApp from './Day 42/Task-10'
+function App() {
+
+
+  return (
+<>
+<h1> NARESH </h1>
+<hr />
+<Counter/>
+<hr />
+<TextDisplay/>
+<hr />
+< Toggole/>
+<hr />
+< EvenOddChecker/>
+<hr />
+<ColorButton/>
+<hr />
+< LoginForm/>
+<hr />
+<ThemeSwitcher/>
+<hr />
+<Counter02/>
+<hr />
+<DropdownMenu/>
+<hr />
+<TodoApp/>
+<hr />
+
+
+</>
+
+
+  )
+}
+
+export default App
